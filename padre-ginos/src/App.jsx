ad88b7e57abf3acadaee80dae6f9685959b25eb5
Sharon K <katz.sharona@gmail.com>
2025-11-01 19:22:04 -0400

    pizzas

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -5,9 +5,9 @@ const App = () => {
     return (
         <div>
             <h1>Padre Gino's - Order Now</h1>
-            <Pizza name="Pepperoni" description="pep, cheese, n stuff" />
-            <Pizza name="Hawaiian" description="ham, pineapple, n stuff" />
-            <Pizza name="American" description="french fries, hot dogs, n stuff" />
+            <Pizza name="Pepperoni" description="pep, cheese, n stuff" image={"/public/pizzas/pepperoni.webp"} />
+            <Pizza name="Hawaiian" description="ham, pineapple, n stuff" image={"/public/pizzas/hawaiian.webp"} />
+            <Pizza name="American" description="french fries, hot dogs, n stuff" image={"/public/pizzas/big_meat.webp"} />
         </div>
     )
 };
